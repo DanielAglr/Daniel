140af9d376d920b52c79725e35f3c875d226d858
DanielAglr <81238732+DanielAglr@users.noreply.github.com>
2022-05-04 23:28:42 -0700

    Updated about info

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -7,7 +7,7 @@ function About() {
                 <div className="section-title">/ about-me</div>
             </div>
             <div className="about-info">
-                <p>I'm currently a High School student interested in the Conputer Science field and am hoping to pursue a Masters in Software Engineering. I'm aspiring to be a part of a big company in either Software Engineering or Data Science where I get the opportunity to use a variety of languages on a frequent basis.</p>
+                <p>I'm currently a High School student interested in the Computer Science field and am hoping to pursue a Masters in Software Engineering. I'm aspiring to be a part of a big company as a Software Engineer where I get the opportunity to use a variety of languages on a frequent basis.</p>
                 <p>I love the logical aspect of software development and the sense of accomplishment from problem solving. Other areas of interest for me include data-visualization using APIs, scripting / automation, and making project websites. In my free time, I like to weight lift and play video games with friends.</p>
                 <div className="about-lang">
                     <span>Here are some languages / technology I've been working with:</span>
