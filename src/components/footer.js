28d2eff92b25389661b9a5f193c320e7d0028811
Daniel <81238732+danielaglr@users.noreply.github.com>
2023-03-28 19:02:23 -0700

    Refactor old code and update information

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,10 @@
 function Footer() {
-    return (
-        <div className='flex flex-col items-center w-full text-contrast-dark text-[15px] text-center pt-[100px]'>
-            <span>Created by Daniel Aguilar.</span>
-            <span>Last edited: 3/7/23.</span>
-        </div>
-    )
+  return (
+    <div className='flex flex-col items-center w-full text-contrast-dark text-[15px] text-center pt-[100px]'>
+      <span>Created by Daniel Aguilar.</span>
+      <span>Last edited: 3/7/23.</span>
+    </div>
+  )
 }
 
 export default Footer;
--- src/components/intro.js
+++ src/components/intro.js
@@ -4,7 +4,7 @@ function Intro() {
       <div className='text-[85px] text-contrast mt-[30vh] md:leading-[70px] md:mt-[20vh]'>
           Hi, I'm <span className='text-accent'>Daniel</span>.
       </div>
-      <div className='max-w-[55%] text-contrast-dark text-[20px] mb-[50px] md:max-w-[85%] md:my-[5vh] '>I'm a student from Long Beach. I'm very interested in full-stack development, data manipulation, and anything involving JavaScript.</div>
+      <div className='max-w-[55%] text-contrast-dark text-[20px] mb-[50px] md:max-w-[85%] md:my-[5vh] '>I'm a student from Long Beach. I'm very interested in full-stack development, data manipulation, and anything involving React.</div>
       <div>
         <a className='flex justify-center items-center w-[150px] h-[50px] text-accent border border-accent rounded-[5px] no-underline' href='mailto:daniel16aglr@gmail.com'>
           <i className='bx bx-conversation text-[20px] pr-[5px]'/>
